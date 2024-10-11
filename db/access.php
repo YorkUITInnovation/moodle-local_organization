d38a9fc90b82c29526d9b5ba7f49ba0bf17b215d
Patrick Thibaudeau <thibaud@yorku.ca>
2024-10-11 07:48:33 -0600

    Added capabilites

---
+++ db/access.php
@@ -0,0 +1,47 @@
+<?php
+$capabilities = array(
+    /**
+     * View only
+     */
+    'local/organization:unit_view' => array(
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        )
+    ),
+    /**
+     * Edit, Delete Campuses, Units, Departments
+     */
+    'local/organization:unit_edit' => array(
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        )
+    ),
+    /**
+     * View advisors
+     */
+    'local/organization:advisor_view' => array(
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        )
+    ),
+    /**
+     * Edit, delete advisors
+     */
+    'local/organization:advisor_edit' => array(
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        )
+    )
+);
